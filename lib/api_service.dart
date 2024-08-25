67e9517f9501f973bba11bdda27396726fb6782a
nivindulakshitha <nivindulakshitha@gmail.com>
2024-08-25 18:05:37 +0530

    chore: Add GET request function to API service

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-// Function to make a POST request
+// Function to make a POST request:nivindulakshitha
 Future<Map<String, dynamic>> postRequest(
     String endpoint, Map<String, dynamic> body) async {
   final url = Uri.parse('https://quizify-api.netlify.app/api/$endpoint');
@@ -30,4 +30,28 @@ Future<Map<String, dynamic>> postRequest(
       'error': error.toString(),
     };
   }
+}
+
+// Function to make a GET request:nivindulakshitha
+Future<Map<String, dynamic>> getRequest(String endpoint) async {
+	final url = Uri.parse('https://quizify-api.netlify.app/api/$endpoint');
+
+	try {
+		final response = await http.get(url);
+
+		if (response.statusCode == 200) {
+			return jsonDecode(response.body);
+		} else {
+			return {
+				'success': false,
+				'message': 'An error occurred while processing your request',
+			};
+		}
+	} catch (error) {
+		return {
+			'success': false,
+			'message': 'An error occurred while processing your request',
+			'error': error.toString(),
+		};
+	}
 }
